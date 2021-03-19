91c75555390c24bd364f68649e03a819d30a2c9d
Hadirizaldi <Hadirizaldi.hr@gmail.com>
2021-03-19 14:59:22 +0700

    Gallery finish dan mulai contact me

--- script.js
+++ script.js
@@ -12,4 +12,26 @@ $(document).ready(function () {
     $(".navbar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
   });
+
+  //   Script untuk aktif owl carousel
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 1,
+        nav: false,
+      },
+    },
+  });
 });
